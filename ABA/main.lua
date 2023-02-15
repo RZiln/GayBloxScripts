05196e824e12f5e2e9b97fcf0f283fec700b1ac9
RZiln <radvilas0@gmail.com>
2023-02-15 21:17:30 +0200

    Attempt on making compatibility

--- ABA/main.lua
+++ ABA/main.lua
@@ -1,11 +1,13 @@
-repeat wait() until game.Loaded
+repeat
+    wait()
+until game.Loaded
 game:GetService("RunService"):Set3dRenderingEnabled(false)
 setfpscap(10)
 task.wait(5)
-
-
 if game.PlaceId == 5411459567 then
-    repeat task.wait(1) until game.Workspace.Message
+    repeat
+        task.wait(1)
+    until game.Workspace.Message
 end
 _G.MinTimer = 60
 
@@ -17,11 +19,21 @@ local function notify(text)
     })
 end
 
-if game.GameId == 578392296 then
-    if game.PlaceId ~= 5411459567 then
-        notify("Joining AFK World")
+local function serverHop()
+    if syn then
         syn.queue_on_teleport(
             'loadstring(game:HttpGet("https://raw.githubusercontent.com/RZiln/GayBloxScripts/master/ABA/main.lua", true))()')
+    else
+        queue_on_teleport(
+            'loadstring(game:HttpGet("https://raw.githubusercontent.com/RZiln/GayBloxScripts/master/ABA/main.lua", true))()')
+    end
+
+end
+
+if game.GameId == 578392296 then
+    if game.PlaceId ~= 5411459567 then
+        notify("Joining AFK World, please")
+        serverHop()
         game:GetService('TeleportService'):Teleport(5411459567)
     end
 
@@ -39,13 +51,11 @@ if game.GameId == 578392296 then
         until timerCheck() > _G.MinTimer
         notify("Server Hopping")
         task.wait(1)
-        syn.queue_on_teleport(
-            'loadstring(game:HttpGet("https://raw.githubusercontent.com/RZiln/GayBloxScripts/master/ABA/main.lua", true))()')
+        serverHop()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/RZiln/GayBloxScripts/master/ABA/ServerHop.lua", true))()
     else
         notify("Server Hopping")
-        syn.queue_on_teleport(
-            'loadstring(game:HttpGet("https://raw.githubusercontent.com/RZiln/GayBloxScripts/master/ABA/main.lua", true))()')
+        serverHop()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/RZiln/GayBloxScripts/master/ABA/ServerHop.lua", true))()
     end
 
