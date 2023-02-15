5caa63792eb634e96e107478eaacd24186cf31a2
RZiln <radvilas0@gmail.com>
2023-02-15 14:12:12 +0200

    fixes 2

--- ABA/main.lua
+++ ABA/main.lua
@@ -1,29 +1,25 @@
 game:GetService("Players").PlayerAdded:Wait()
-
 task.wait(5)
+print("executed")
 if game.PlaceId == 5411459567 then
-repeat task.wait(1)
-    
-until game.Workspace.Message
+    repeat task.wait(1) until game.Workspace.Message
 end
 _G.MinTimer = 60
 
-
-
 local function notify(text)
     game.StarterGui:SetCore("SendNotification", {
-		Title = "RZiln's AFK hop";
-        Duration = 10;
-		Text = text
-	})
+        Title = "RZiln's AFK hop",
+        Duration = 10,
+        Text = text
+    })
 end
 
 if game.GameId == 578392296 then
-            
-    
+    print("Running")
     if game.PlaceId ~= 5411459567 then
         notify("Joining AFK World")
-        syn.queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/RZiln/GayBloxScripts/master/ABA/main.lua", true))()')
+        syn.queue_on_teleport(
+            'loadstring(game:HttpGet("https://raw.githubusercontent.com/RZiln/GayBloxScripts/master/ABA/main.lua", true))()')
         game:GetService('TeleportService'):Teleport(5411459567)
     end
     notify("AFK Hop running")
@@ -35,16 +31,19 @@ if game.GameId == 578392296 then
 
     if timerCheck() <= _G.MinTimer then
         notify("Waiting for reward")
-        repeat task.wait() until timerCheck() > _G.MinTimer
+        repeat
+            task.wait()
+        until timerCheck() > _G.MinTimer
         notify("Server Hopping")
         task.wait(1)
-        syn.queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/RZiln/GayBloxScripts/master/ABA/main.lua", true))()')
+        syn.queue_on_teleport(
+            'loadstring(game:HttpGet("https://raw.githubusercontent.com/RZiln/GayBloxScripts/master/ABA/main.lua", true))()')
         loadstring(game:HttpGet("https://raw.githubusercontent.com/RZiln/GayBloxScripts/master/ABA/ServerHop.lua", true))()
     else
         notify("Server Hopping")
-        syn.queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/RZiln/GayBloxScripts/master/ABA/main.lua", true))()')
+        syn.queue_on_teleport(
+            'loadstring(game:HttpGet("https://raw.githubusercontent.com/RZiln/GayBloxScripts/master/ABA/main.lua", true))()')
         loadstring(game:HttpGet("https://raw.githubusercontent.com/RZiln/GayBloxScripts/master/ABA/ServerHop.lua", true))()
     end
-
 
 end
